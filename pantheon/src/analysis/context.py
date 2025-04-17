445dd3086042667818a80207ccbad0ecb6f9e4e5
xxender13 <hs5189@srmist.edu.in>
2025-04-17 17:08:06 -0500

    Part b & c result

--- pantheon/src/analysis/context.py
+++
@@ -1,5 +0,0 @@
-import os
-from os import path
-import sys
-src_dir = path.abspath(path.join(path.dirname(__file__), os.pardir))
-sys.path.append(src_dir)
--- pantheon/src/experiments/context.py
+++
@@ -1,6 +0,0 @@
-import os
-from os import path
-import sys
-src_dir = path.abspath(path.join(path.dirname(__file__), os.pardir))
-base_dir = path.abspath(path.join(src_dir, os.pardir))
-sys.path.append(src_dir)
